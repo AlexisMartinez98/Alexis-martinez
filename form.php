0131b07d4c1ad4d426bdffe1f258f8b720a7ba13
Alexis Martinez <as.alexismartinez@gmail.com>
2022-04-10 16:32:35 -0300

    agrego archivo php y index de exito de form

---
+++ form.php
@@ -0,0 +1,19 @@
+<?php
+    // para capturar la info que se ingresa en los inputos del form tengo que crear variables.
+
+    $nombre = $_POST ['nombre'];
+    $mail = $_POST ['email'];
+    $mensaje = $_POST['textarea'];
+    
+    // a quien le mando esta info
+    $para = 'alexiscabj74@gmail.com';
+    $asunto = 'este mail fue enviado desde la web';
+
+    // funcion que envia la info del form
+    mail($para, $asunto, utf8_decode($mensaje));
+
+    // funcion que redirreciona al usaria una vez que completo el form
+
+    header('location:exito.html');
+
+?>
